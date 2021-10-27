567ddb7a76bf52d89a47495b78b6c1c9a76886b4
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-27 16:20:31 +0800

    Initial Commit

---
+++ Drawing_Challenge/Drawing_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_ChallengeApp.swift
+//  Drawing_Challenge
+//
+//  Created by Giovanni Gaffé on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
