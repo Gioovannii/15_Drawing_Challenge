bd01b43266351e75eeaa4e2f08ed44e1819dc0f4
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-28 21:40:24 +0800

    Add color cycling rectangle to transform shape to animated

--- Drawing_Challenge/ContentView.swift
+++ Drawing_Challenge/ContentView.swift
@@ -7,6 +7,32 @@
 
 import SwiftUI
 
+
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+            }
+        }
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct Arrow: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -24,6 +50,8 @@ struct Arrow: Shape {
 
 struct ContentView: View {
     @State private var thickness = 3.0
+    @State private var colorCycle = 0.0
+
     var body: some View {
         VStack {
             Arrow()
@@ -33,11 +61,15 @@ struct ContentView: View {
                 .frame(width: 100, height: 300)
                 .border(.black, width: 3)
                 .foregroundColor(.white)
-                .offset(x: -13, y: -143)
+                .offset(x: -13, y: -87)
     
             Text("Thickness : \(thickness, specifier: "%.2g")")
             Slider(value: $thickness, in: 3...15)
-                .padding()
+                .padding([.horizontal, .bottom])
+            
+            Text("Color : \(colorCycle, specifier: "%.2g")")
+            Slider(value: $colorCycle)
+                .padding(.horizontal)
             
         }
     }
